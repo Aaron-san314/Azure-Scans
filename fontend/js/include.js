253d4bf04f112eaf9a662802d070b0534794e65b
aaqib <168923599+Aaron-san314@users.noreply.github.com>
2025-05-28 19:56:15 +0530

    signup and login pagesgit commit

---
+++ fontend/js/include.js
@@ -0,0 +1,13 @@
+function includeHTML() {
+    const elements = document.querySelectorAll('[data-include]');
+    elements.forEach(el => { // use `el` consistently
+        const file = el.getAttribute('data-include');
+        fetch(file)
+            .then(res => res.text())
+            .then(data => {
+                el.innerHTML = data;
+            });
+    });
+}
+
+window.addEventListener('DOMContentLoaded', includeHTML);
